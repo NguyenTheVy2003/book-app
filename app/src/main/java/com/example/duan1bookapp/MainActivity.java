454e6a528b8652fc82d825478e2a2666e15580be
anphu123 <anphu221002@gmail.com>
2023-07-03 02:57:05 +0700

    phu

--- app/src/main/java/com/example/duan1bookapp/MainActivity.java
+++ app/src/main/java/com/example/duan1bookapp/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.duan1bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.duan1bookapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // handle loginBtn click, start login screen
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+        // handle skipBtn click, start continue without login screen
+        binding.skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, DashboardUserActivity.class));
+            }
+        });
     }
 }
