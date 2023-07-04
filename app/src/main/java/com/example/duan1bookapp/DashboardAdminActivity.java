7f122c28b908cc6d0cdaa8c6517ebacc4f0b1778
taficor <125031119+taficor@users.noreply.github.com>
2023-07-04 15:28:22 +0700

    Video3

--- app/src/main/java/com/example/duan1bookapp/DashboardAdminActivity.java
+++ app/src/main/java/com/example/duan1bookapp/DashboardAdminActivity.java
@@ -36,7 +36,18 @@ public class DashboardAdminActivity extends AppCompatActivity {
                 checkUser();
             }
         });
+
+        //handle click, start   category ad screen
+        binding.addCategoryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashboardAdminActivity.this,CategoryAddActivity.class));
+            }
+        });
     }
+
+
+
 
     private void checkUser() {
         //get current user
