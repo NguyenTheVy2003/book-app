10302b92a93172e3d562fc119afc30b18b3f2174
taficor <125031119+taficor@users.noreply.github.com>
2023-07-05 22:50:09 +0700

    Video4

--- app/src/main/java/com/example/duan1bookapp/DashboardAdminActivity.java
+++ app/src/main/java/com/example/duan1bookapp/DashboardAdminActivity.java
@@ -1,14 +1,24 @@
 package com.example.duan1bookapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
 import com.example.duan1bookapp.databinding.ActivityDashboardAdminBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardAdminActivity extends AppCompatActivity {
 
@@ -17,6 +27,10 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
     //firebase auth
     private FirebaseAuth firebaseAuth;
+
+    //arraylist to story category
+    private ArrayList<ModelCategory>categoryArrayList;
+    private AdapterCategory adapterCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,31 @@ public class DashboardAdminActivity extends AppCompatActivity {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
+        loadCategories();
+
+        //edit text change listen, search
+        binding.searchEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+            //called as and when user type each letter
+                try {
+                    adapterCategory.getFilter().filter(s);
+                }
+                catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         // handle click, logout
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +85,34 @@ public class DashboardAdminActivity extends AppCompatActivity {
         });
     }
 
+    private void loadCategories() {
 
+        //init  arraylist
+        categoryArrayList = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //clear arraylist before adding data into it
+                categoryArrayList.clear();
+                for (DataSnapshot ds:snapshot.getChildren()){
+                    //get data
+                    ModelCategory mode = ds.getValue(ModelCategory.class);
+                    // add to arraylist
+                    categoryArrayList.add(mode);
+                }
+                //setup adapter
+                adapterCategory = new AdapterCategory(DashboardAdminActivity.this,categoryArrayList);
+                //set adapter to recyclerview
+                binding.categoriesRv.setAdapter(adapterCategory);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 
 
     private void checkUser() {
